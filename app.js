ae4b7843b08786f4495b2cd9094171d003450af8
Bayoura <Kristin.Seidelmann@gmx.de>
2021-09-13 17:36:11 +0200

    add grid size changing function

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const container_div = document.querySelector('.container');
+
+function changeGridSize(userInput) {
+    for (let i = 0; i < userInput * userInput; i++) {
+        const gridItem_div = document.createElement('div');
+        gridItem_div.className = 'grid-item';
+        container_div.appendChild(gridItem_div);
+    }
+    container_div.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+}
+
+changeGridSize(16);
+
+const gridItemList_div = document.querySelectorAll('.grid-item');
+const gridItem_div = document.querySelector('.grid-item');
+
 
