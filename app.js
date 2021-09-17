3fe065fdda73c3e5499c364e9745706ca5941366
Bayoura <Kristin.Seidelmann@gmx.de>
2021-09-17 16:16:02 +0200

    add drawing features

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const container_div = document.querySelector('.container');
+let isClicked = false;
 
 function changeGridSize(userInput) {
     for (let i = 0; i < userInput * userInput; i++) {
@@ -14,4 +15,35 @@ changeGridSize(16);
 const gridItemList_div = document.querySelectorAll('.grid-item');
 const gridItem_div = document.querySelector('.grid-item');
 
+//---------------------------------------MOUSEDOWN PEN------------------------------------------- 
+container_div.addEventListener('mousedown', () => isClicked = true);
+container_div.addEventListener('mouseup', () => isClicked = false); 
+
+ gridItemList_div.forEach(gridItem_div => gridItem_div.addEventListener('mousemove', () => {
+     if (isClicked) gridItem_div.style.background = 'black';   
+    }));
+//---------------------------------------TOGGLE PEN-------------------------------------------  
+
+// container_div.addEventListener('click',() => togglePainting() );
+
+// function togglePainting() { 
+//     if (!isClicked) yay();  
+//     else nay();      
+// }
+
+// function yay() {
+//     gridItemList_div.forEach(gridItem_div => gridItem_div.addEventListener('mouseover', penActive));
+//     isClicked = true;
+//     console.log(isClicked);
+// }
+
+// function nay() {
+//     gridItemList_div.forEach(gridItem_div => gridItem_div.removeEventListener('mouseover', penActive));       
+//     isClicked = false;      
+//     console.log(isClicked);
+// }
+
+// function penActive(e) {
+//     e.target.style = 'background-color: black';
+// }
 
