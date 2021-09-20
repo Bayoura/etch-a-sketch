6ef845e2c674fb049165b7f9f3f749de5ee06ab8
Bayoura <Kristin.Seidelmann@gmx.de>
2021-09-20 14:51:13 +0200

    add color picker tool and eraser button, black button and random button

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const container_div = document.querySelector('.container');
+const options_div = document.querySelector('.options');
+const colorPicker_input = document.getElementById('colorPicker');
+const eraser_button = document.getElementById('eraser');
+const black_button = document.getElementById('black');
+const random_button = document.getElementById('random');
+const defaultColor = 'black';
+
+let currentColor = defaultColor;
 let mouseDown = false;
 
 function changeGridSize(userInput) {
@@ -15,12 +23,37 @@ changeGridSize(16);
 const gridItemList_div = document.querySelectorAll('.grid-item');
 const gridItem_div = document.querySelector('.grid-item');
 
-//---------------------------------------MOUSEDOWN PEN------------------------------------------- 
+//---------------------------------------DRAWING-FUNCTIONS-------------------------------------------
+
 container_div.addEventListener('mousedown', () => mouseDown = true);
 container_div.addEventListener('mouseup', () => mouseDown = false); 
- 
+
+function setColor(color) {
+    currentColor = color;
+}
+
+colorPicker_input.oninput = () => setColor(colorPicker_input.value);
+eraser_button.onclick = () => setColor('white');
+black_button.onclick = () => setColor('black');
+random_button.onclick = () => setColor(getRandomColor());
+
 gridItemList_div.forEach(gridItem_div => gridItem_div.addEventListener('mousemove', () => {    
-    if (mouseDown) gridItem_div.style.background = 'black';      
+    if (mouseDown) gridItem_div.style.background = currentColor;      
 }));
    
-gridItemList_div.forEach(gridItem_div => gridItem_div.addEventListener('click', () => gridItem_div.style.background = 'black'));
+gridItemList_div.forEach(gridItem_div => gridItem_div.addEventListener('click', () => gridItem_div.style.background = currentColor));
+
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let randomColor = `rgb(${r}, ${g}, ${b})`;
+    return randomColor;
+}
+
+// gridItemList_div.forEach(gridItem_div => gridItem_div.addEventListener('mousemove', () => {    
+//     if (mouseDown) gridItem_div.style.background = colorInput();      
+// }));
+   
+// gridItemList_div.forEach(gridItem_div => gridItem_div.addEventListener('click', () => gridItem_div.style.background = colorInput()));
+
