228d08937ffb1f2a702d5aa9b30c39d498f19d53
Bayoura <Kristin.Seidelmann@gmx.de>
2021-09-24 13:09:22 +0200

    change grid size with range slider

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const container_div = document.querySelector('.container');
+const sizeValue_p = document.querySelector('.size-value');
+const sizeRange_input = document.getElementById('size-range');
 const colorPicker_input = document.getElementById('color-picker');
 const black_button = document.getElementById('black');
 const eraser_button = document.getElementById('eraser');
@@ -8,8 +10,12 @@ const clear_button = document.getElementById('clear-grid');
 const toggleBorders_button = document.getElementById('toggle-borders');
 const defaultColor = 'black';
 
+let gridItemList_div;
+let gridItem_div;
 let currentColor = defaultColor;
 let mouseDown = false;
+
+//---------------------------------------GRID-------------------------------------------
 
 function changeGridSize(userInput) {
     for (let i = 0; i < userInput * userInput; i++) {
@@ -18,12 +24,20 @@ function changeGridSize(userInput) {
         container_div.appendChild(gridItem_div);
     }
     container_div.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+    gridItemList_div = document.querySelectorAll('.grid-item');
+    gridItem_div = document.querySelector('.grid-item');
 }
 
-changeGridSize(16);
+sizeRange_input.addEventListener('input', () => {
+    removeGridItems();
+    changeGridSize(sizeRange_input.value);
+    sizeValue_p.textContent = sizeRange_input.value + ' x ' + sizeRange_input.value;   
+    draw();
+})
 
-const gridItemList_div = document.querySelectorAll('.grid-item');
-const gridItem_div = document.querySelector('.grid-item');
+let removeGridItems = () => container_div.innerHTML = '';
+
+changeGridSize(16);
 
 //---------------------------------------DRAWING-FUNCTIONS-------------------------------------------
 
@@ -59,5 +73,4 @@ toggleBorders_button.onclick = () => gridItemList_div.forEach(gridItem_div => gr
 
 draw();
 
-//more features: scale grid
 //test different browser, add browser prefix (user-select and transition);
